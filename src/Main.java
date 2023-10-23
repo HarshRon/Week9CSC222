d5ceb36749dfd3c443fa495eb9d242c24fd70225
10student <10student@nr.stu>
2023-10-23 10:05:53 -0400

    Array practice

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args){
         int[] codes;
@@ -11,5 +12,63 @@ public class Main {
     for(i=0;i <5;++i) {
         System.out.println(codes[i]);
     }
+        int[] codesAgain = {24, 36, 48, 51, 62};
+        double[] sales = new double[10];
+        System.out.println("Some codes: " + codesAgain[0] + "  " + codesAgain[1]);
+        System.out.println("Some sales: " + sales[0] + "  "  + sales[9]);
+
+        int[] codesWow = {24, 36, 48, 51, 62};
+        int x;
+        System.out.println("Forwards:");
+        for(x = 0; x < codesWow.length; ++x)
+            System.out.print(codesWow[x] + "   ");
+        System.out.println("\nBackwards:");
+        for(x = codesWow.length - 1; x >=0; --x)
+            System.out.print(codesWow[x] + "   ");
+        int[] zipCodes = {14903,24060,14901,23456,24053};
+        System.out.println("\n" + zipCodes[2]);
+
+
+
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        int[] days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String monthEntered;
+        int y;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a three-letter month name abbreviation >> ");
+        monthEntered = in.nextLine();
+        for(y = 0; y < months.length; ++y)
+            if(months[y].equals(monthEntered))
+                System.out.println(months[y] + " can have " + days[y] + " days");
+
+
+        String[] bananas = new String[10];
+        for(int bananaCount = 0; bananaCount < bananas.length; ++bananaCount) {
+            bananas[bananaCount] = "Bananananana! " + bananaCount;
+            System.out.println(bananas[bananaCount]);
+        }
+
+
+        int[] scores = {76, 82, 90, 75, 92, 88};
+        int z;
+        System.out.println("Before giving each score a bonus:");
+        for(z= 0; z < scores.length; ++z)
+            System.out.print(scores[z] + "  ");
+        System.out.println();
+        giveBonus(scores);
+        System.out.println("After giving each score a bonus:");
+        for(z = 0; z < scores.length; ++z)
+            System.out.print(scores[z] + "  ");
+        System.out.println();
+
+
+    }
+    public static void giveBonus(int[] testScores)
+    {
+        int i;
+        final int ADJUSTMENT = 6;
+        for(i = 0; i < testScores.length; ++ i)
+            testScores[i] += ADJUSTMENT;
     }
 }
---
+++ src/Product.java
@@ -0,0 +1,17 @@
+public class Product {
+    private int itemNum;
+    private double price;
+    Product(int num, double pr)
+    {
+        itemNum = num;
+        price = pr;
+    }
+    public int getItemNum()
+    {
+        return itemNum;
+    }
+    public double getPrice()
+    {
+        return price;
+    }
+}
