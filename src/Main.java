371146e5ff8f56aaf4cfd14ee1673db638e5de07
10student <10student@nr.stu>
2023-10-23 09:19:14 -0400

    Weel 9 notebook start

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        int[] codes;
+        codes = new int[5];
+        codes[0] = 321;
+        codes[1]=432;
+        codes[2]=543;
+        codes[3]=654;
+        codes[4]=765;
+        int i;
+    for(i=0;i <5;++i) {
+        System.out.println(codes[i]);
+    }
+    }
+}
